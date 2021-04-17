1d6d77dc844a6d019f51f9fe40f152fad2216db9
Orlando Grimany <ogrimany@Orlandos-MacBook-Air.local>
2021-04-17 09:17:24 -0400

    updated public folder dir

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/pdf', pdfRouter);
 
+app.use(express.static(__dirname+'/public'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
